78e3e644e75d374259f65ee1b2f79ef83b764fd8
arafatmansuri <mansuriarafat302@gmail.com>
2025-12-29 17:19:37 +0530

    add user router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import cookieParser from "cookie-parser";
 import express from "express";
+import { userRouter } from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello, StudyNotion!");
 });
+
+app.use("/api/users",userRouter)
 
 export default app;
