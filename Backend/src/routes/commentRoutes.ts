9ebeeed999c800cdfc2c42055598b29bd175b8fc
arafatmansuri <mansuriarafat302@gmail.com>
2026-02-22 20:11:34 +0530

    Update comment routes

--- Backend/src/routes/commentRoutes.ts
+++ Backend/src/routes/commentRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createComment, deleteComment, getCommentsBySubSectionId, updateComment } from "../controllers/commentController.js";
+import { userMiddleware } from "../middlewares/userMiddleware.js";
 
 const CommentRouter = Router();
 
+CommentRouter.use(userMiddleware);
 CommentRouter.route("/add").post(createComment);
 CommentRouter.route("/get/:subsectionId").get(getCommentsBySubSectionId);
 CommentRouter.route("/update/:commentId").put(updateComment);
