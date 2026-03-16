8f8dbef4e816a743d709a834b86f0478e1fcd059
Parth Chauhan <parthchauhan25125@gmail.com>
2026-03-16 13:43:03 +0530

    Origin Frontend Url Updated

--- Frontend/src/components/Routes/AppRoutes.jsx
+++ Frontend/src/components/Routes/AppRoutes.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import { ACCOUNT_TYPE } from '../../utils/constants'
+import {Route,Routes} from "react-router-dom"
+import { Home } from '../../pages/Home';
+import { Login } from '../core/Login';
+import CatalogItem from '../core/CatalogItem';
+import { Signup } from '../core/signup';
+import ForgotPassword from '../../pages/ForgotPassword';
+import UpdatePassword from '../../pages/UpdatePassword';
+import VerifyEmail from '../../pages/VerifyEmail';
+import About from '../../pages/About';
+import ContactUs from '../../pages/ContactUs';
+import Dashboard from '../../pages/Dashboard';
+import MyProfile from '../core/Dashboard/MyProfile';
+import OpenRoute from '../core/Auth/OpenRoute';
+import PrivateRoute from '../core/Auth/PrivateRoute';
+import Error from '../../pages/Error';
+import Settings from '../common/Settings';
+import EnrolledCourses from '../../pages/EnrolledCourses';
+import Cart from '../core/Auth/Cart';
+import { ACCOUNT_TYPE } from '../../utils/constants';
 import { useSelector } from 'react-redux';
+import MyCourses from '../Instructor/MyCourses';
+import AddCourse from '../core/Dashboard/Add-Course/AddCourse';
 
 const AppRoutes = () => {
     const {user}=useSelector((state)=>state.profile);
