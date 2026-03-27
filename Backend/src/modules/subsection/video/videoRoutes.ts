4ef9160dde65fb2d821201780030383178c783a4
arafatmansuri <mansuriarafat302@gmail.com>
2026-03-27 17:17:03 +0530

    Update save video progress route

--- Backend/src/modules/subsection/video/videoRoutes.ts
+++ Backend/src/modules/subsection/video/videoRoutes.ts
@@ -10,6 +10,6 @@ const videoRouter = Router();
 // videoRouter.route("/multipart/:uploadId/batch").get(videoBatchHandler);
 
 videoRouter.route("/getone/:subsectionId").get(getVideo);
-videoRouter.route("/saveprogress/:subsectionId").post(saveVideoProgress);
+videoRouter.route("/saveprogress").post(saveVideoProgress);
 
 export default videoRouter;
