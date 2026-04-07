996b3b73aeca9a384303d74637f4a9a643c461af
arafatmansuri <mansuriarafat302@gmail.com>
2026-04-07 23:37:06 +0530

    Add markComplete subsection functionality

---
+++ frontend/src/services/operations/subsectionAPI.js
@@ -0,0 +1,21 @@
+import toast from "react-hot-toast";
+import { subSectionVideoEndpoints } from "../apis";
+import { apiConnector } from "../apiconnector";
+
+const {PUT_MARK_SUBSECTION_COMPLETED_API} = subSectionVideoEndpoints;
+export const markSubsectionAsCompleted = async (subsectionId) => {
+  const toastId = toast.loading("Marking subsection as completed...");
+  let result = null;
+  try {
+    const url = PUT_MARK_SUBSECTION_COMPLETED_API.replace(":subsectionId", subsectionId);
+    const response = await apiConnector("PUT", url);
+    if (!response?.data?.success) throw new Error("Could not mark subsection as completed");
+    toast.success("Subsection marked as completed");
+    result = response.data.data;
+  } catch (error) {
+    toast.error(error.message || "Failed to mark subsection as completed");
+  } finally {
+    toast.dismiss(toastId);
+  }
+  return result;
+};
